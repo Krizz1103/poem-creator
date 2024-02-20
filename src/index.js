383661ce76fcf54124358641ca28cff769e35076
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2024-02-20 20:29:51 +0000

    Added the API

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
+function displayPoem(response) {
+  new Typewriter("#poem", {
+    strings: response.data.answer,
+    autoStart: true,
+  });
+}
+
 function generatePoem(event) {
   event.preventDefault();
 
-  new Typewriter("#poem", {
-    strings: "Here is the poem",
-    autoStart: true,
-  });
+  let instructionInput = document.querySelector("#user-instructions");
+  let apiKey = "54734t1680o470cbbe3f700fdd2fa18e";
+  let prompt = `creat a funny poem about ${instructionInput.value}`;
+  let context =
+    "you are a funny poem artist and like to write short 4 line poems";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#generator-form");
