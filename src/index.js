a565458cad6854989840d3ecae37290d9bdb4332
Krizz1103 <132948667+Krizz1103@users.noreply.github.com>
2024-02-20 19:58:04 +0000

    Added JS and typewriter pluggin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings: "Here is the poem",
+    autoStart: true,
+  });
+}
+
+let poemFormElement = document.querySelector("#generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
